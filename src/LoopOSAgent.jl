44a9b085a36c4ee0cfc5f13630ab61fac619ffcc
1m1 <imi@1m1.io>
2026-04-24 11:03:45 -0600

    update

--- src/LoopOSAgent.jl
+++ src/LoopOSAgent.jl
@@ -8,18 +8,18 @@ function start(name, group, textrouterlocation, textpublocation, togcreatelocati
     LoopOSAgentCommunication.start(togcreatelocation, togobservelocation)
 end
 
-using StaticArrays
-using TheoryOfGod: T
-using TheoryOfGodgod: god
-const invϕ = one(T) / MathConstants.golden
-const GOD = god(;
-    ω = Ω,
-    d = SA[invϕ, invϕ^2, invϕ^3],
-    t = t(),
-    ẑeroμ = SA[○, ○, ○],
-    ôneμ = SA[○, ○, ○],
-    ρ = (T(0.01),T(0.01),T(0.01)),
-    ♯ = (10^3, 10^3),
-)
+# using StaticArrays
+# using TheoryOfGod: T
+# using TheoryOfGodgod: god
+# const invϕ = one(T) / MathConstants.golden
+# const GOD = god(;
+#     ω = Ω,
+#     d = SA[invϕ, invϕ^2, invϕ^3],
+#     t = t(),
+#     ẑeroμ = SA[○, ○, ○],
+#     ôneμ = SA[○, ○, ○],
+#     ρ = (T(0.01),T(0.01),T(0.01)),
+#     ♯ = (10^3, 10^3),
+# )
 
 end
